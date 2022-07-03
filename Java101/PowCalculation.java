6a1e2877ec1d074b446131d67d8d8f294329fb01
HasanCeylan <70063987+HasanOzCeylan@users.noreply.github.com>
2022-07-03 23:04:43 +0300

    Add files via upload

---
+++ Java101/PowCalculation.java
@@ -0,0 +1,22 @@
+package Java101;
+import java.util.Scanner;
+
+public class PowCalculation {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number for base: ");
+        int number = input.nextInt();
+        System.out.print("Enter a number for exponent: ");
+        int times = input.nextInt();
+        int total = 1;
+
+        for (int i = 1; i <= times; i++) {
+            total *= number;
+        }
+        System.out.print(total);
+
+    }
+}
+
